ae114c12721a820bf9d94fc60082f7d2225a5a45
FumiakiTono <tonotorious@gmail.com>
2016-11-16 13:33:47 +0900

    Create routing at groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,16 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(create_params)
+    redirect_to controller: :messages, action: :index
+  end
+
+  private
+  def create_params
+    params.permit(:name)
+  end
+
 end
