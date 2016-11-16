1e7cd0d37fdeb7fce43c071b5c045454eecd0060
FumiakiTono <tonotorious@gmail.com>
2016-11-16 17:33:06 +0900

    Add error message at validation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,13 +4,17 @@ class GroupsController < ApplicationController
   end
 
   def create
-    Group.create(create_params)
-    redirect_to controller: :messages, action: :index
+    @group = Group.new(create_params)
+    if @group.save
+      redirect_to controller: :messages, action: :index
+    else
+      redirect_to controller: :groups, action: :new
+    end
   end
 
   private
   def create_params
-    params.permit(:name)
+    params.require(:chat_group).permit(:name)
   end
 
 end
