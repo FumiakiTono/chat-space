2479333063dfe077a5084ccb3bf1ff2fbda41a45
FumiakiTono <tonotorious@gmail.com>
2016-11-18 18:21:48 +0900

    Change form to form_for

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,13 +8,30 @@ class GroupsController < ApplicationController
     if @group.save
       redirect_to controller: :messages, action: :index
     else
-      redirect_to controller: :groups, action: :new
+      render action: :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(update_params)
+      redirect_to controller: :messages, action: :index
+    else
+      render action: :edit
     end
   end
 
   private
   def create_params
-    params.require(:chat_group).permit(:name)
+    params.require(:group).permit(:name)
+  end
+
+  def update_params
+    params.require(:group).permit(:name)
   end
 
 end
