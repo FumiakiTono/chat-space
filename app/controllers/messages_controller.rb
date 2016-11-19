0af4d10a29d20e0d007d94914c784368d4a6bd90
FumiakiTono <tonotorious@gmail.com>
2016-11-19 17:01:26 +0900

    Add the method of index and create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,18 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.all
+    @message = Message.new
   end
+
+  def create
+    @message = Message.new(create_params)
+    @message.save
+    redirect_to action: :index
+  end
+
+  private
+  def create_params
+    params.require(:message).permit(:body)
+  end
+
 end
