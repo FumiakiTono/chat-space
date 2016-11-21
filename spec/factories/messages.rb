455a27b7e2fabdb0ef5039e1838ee00a6742fafd
FumiakiTono <tonotorious@gmail.com>
2016-11-21 17:22:58 +0900

    Change placeholdit to avatar

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :message do
     body     { Faker::Lorem.sentence }
-    image    { Faker::Placeholdit.image }
+    image    { Faker::Avatar.image }
     group_id "0"
     user_id  "1"
   end
