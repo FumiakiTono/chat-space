e6da644da2f75b79ab8cf7a2f421950aaf6dc23a
FumiakiTono <tonotorious@gmail.com>
2016-11-23 20:49:39 +0900

    Create messages.js file

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,22 @@
+$(function(){
+  $(".message-form").on("submit", function(){
+    var form = $(".message-form").get(0);
+    var formData = new FormData(form)
+    $.ajax({
+      url: "/messages.json",
+      type: "POST",
+      dataType: "json",
+      data: formData,
+      processData: false,
+      contentType: false
+    })
+      .done(function(data){
+        var message = "<div class = 'message-written'><div class = 'message-user'>" + data.name + "</div><div class = 'message-time'>" + data.created_at + "</div><div class = 'message-text'>" + data.body + "</div></div>"
+        $(".chat__message--written").append(message);
+        $(".message-type").val(" ");
+      })
+      .fail(function(data){
+
+      });
+  });
+});
