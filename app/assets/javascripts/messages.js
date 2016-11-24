ca54a2b3f2bc80eb789372e6c17a056c2d0ad6d6
FumiakiTono <tonotorious@gmail.com>
2016-11-24 17:14:05 +0900

    Add e.preventDefault and alert, and change the line

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,5 +1,6 @@
 $(function(){
-  $(".message-form").on("submit", function(){
+  $(".message-form").on("submit", function(e){
+    e.preventDefault();
     var form = $(".message-form").get(0);
     var formData = new FormData(form)
     $.ajax({
@@ -11,12 +12,23 @@ $(function(){
       contentType: false
     })
       .done(function(data){
-        var message = "<div class = 'message-written'><div class = 'message-user'>" + data.name + "</div><div class = 'message-time'>" + data.created_at + "</div><div class = 'message-text'>" + data.body + "</div></div>"
+        var message =
+          "<div class = 'message-written'>" +
+            "<div class = 'message-user'>"  +
+              data.name                     +
+            "</div>"                        +
+            "<div class = 'message-time'>"  +
+              data.created_at               +
+            "</div>"                        +
+            "<div class = 'message-text'>"  +
+              data.body                     +
+            "</div>"                        +
+          "</div>"
         $(".chat__message--written").append(message);
         $(".message-type").val(" ");
       })
       .fail(function(data){
-
+        alert("メッセージが空欄のため保存されませんでした。")
       });
   });
 });
