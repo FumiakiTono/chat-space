375a1dde3004bf6dd63b812c135377db6fc8f9c5
FumiakiTono <tonotorious@gmail.com>
2016-11-24 19:56:00 +0900

    Change the content at format.json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,27 +4,15 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
-
   def create
     @message = Message.new(create_params)
     respond_to do |format|
       if @message.save
-        # format.html { redirect_to @message }
         format.json {
-          render json: {
-            name: @message.user.name,
-            body: @message.body,
-            user_id: @message.user_id,
-            created_at: @message.created_at,
-            image: @message.image,
-            updated_at: @message.updated_at,
-            group_id: @message.group_id,
-            id: @message.id
-          }
+          render json: @message.create_hash(@message)
         }
       else
-        format.html { redirect_to root_path }
-        # format.json { render json: @message.errors }
+        format.json { redirect_to root_path, alert: "メッセージを入力してください。" }
       end
     end
   end
