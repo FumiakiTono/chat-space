1c8ddb2bc1610d7f3aef2ffa2f5ca55db8bbdf99
FumiakiTono <tonotorious@gmail.com>
2016-11-25 17:07:11 +0900

    Delete comment out at group.js file

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,8 +1,6 @@
 $(function(){
   $("#group_id").on("keyup", function(e){
     e.preventDefault();
-    // var form = $("#group_id").get(0);
-    // var formData = new FormData(form);
     function addHtml(data){
       member =
         "<p class = 'chat-group-user__name'>" +
@@ -10,21 +8,12 @@ $(function(){
         "</p>"
       return member;
     }
-    $.ajax({
-      // url: "/groups/new.json",
-      // type: "POST",
-      // dataType: "json",
-      // data: formData,
-      // processData: false,
-      // contentType: false
-    })
+    $.ajax()
       .done(function(){
         var input = $("#group_id").val();
         $("#chat-group-users").append(addHtml(input));
-        // $(".message-type").val(" ");
-        // $("#chat-group-users").remove();
-      });
-      fail(function(){
+      })
+      .fail(function(){
         alert("エラーが発生しました。");
       });
     return false;
