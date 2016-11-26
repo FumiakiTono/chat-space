8194ef67ec186cc479f8d2a182d6836bda0ee336
FumiakiTono <tonotorious@gmail.com>
2016-11-26 16:54:15 +0900

    Add ajax when adding member.

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,19 +1,31 @@
 $(function(){
-  $("#group_id").on("keyup", function(e){
+  function addHtml(name){
+    member =
+      "<p class = 'chat-group-user__name'>"   +
+        name                             +
+      "</p>"
+    return member;
+  }
+  $("#group_user_ids").on("keyup", function(e){
     e.preventDefault();
-    function addHtml(data){
-      member =
-        "<p class = 'chat-group-user__name'>" +
-          data                                +
-        "</p>"
-      return member;
-    }
-    $.ajax()
-      .done(function(){
-        var input = $("#group_id").val();
-        $("#chat-group-users").append(addHtml(input));
+    $.ajax({
+      url: "/groups/new.json",
+      type: "GET",
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+      .done(function(data){
+        input = $("#group_user_ids").val();
+        for(var i = 0; i< data.length; i = i+1 ){
+          name = data[i].name
+          if(name == input){
+            $("#chat-group-users").append(addHtml(data[i].name));
+            return false;
+          }
+        }
       })
-      .fail(function(){
+      .fail(function(data){
         alert("エラーが発生しました。");
       });
     return false;
