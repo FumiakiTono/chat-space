80d5ad283bc64745051e464db1764b3c188455a9
FumiakiTono <tonotorious@gmail.com>
2016-11-26 16:48:39 +0900

    Add the name column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121072424) do
+ActiveRecord::Schema.define(version: 20161126073259) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
