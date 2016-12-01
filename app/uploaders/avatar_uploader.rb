db807d9be245cde58ac325e3358b8abc59cac2a9
FumiakiTono <tonotorious@gmail.com>
2016-12-01 12:24:02 +0900

    Create uploaders

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+class AvatarUploader < CarrierWave::Uploader::Base
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  uploader = AvatarUploader.new
+  uploader.store!(my_file)
+  uploader.retrieve_from_store!("my_file.png")
+
+end
