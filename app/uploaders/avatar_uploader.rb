5c691f1c3b732de0a4bb0d4da109a114d4aebd30
FumiakiTono <tonotorious@gmail.com>
2016-12-01 12:56:24 +0900

    Delete the processing of uploader at avatar_uproader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -8,8 +8,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  uploader = AvatarUploader.new
-  uploader.store!(my_file)
-  uploader.retrieve_from_store!("my_file.png")
-
 end
