d0d877f2bcc0ee6f7cdfd74ed43bf4f4e73a566b
FumiakiTono <tonotorious@gmail.com>
2016-12-02 16:51:20 +0900

    Create carrierwave file

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,27 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => ENV['AWS_REGION'],
+      :path_style             => true,
+  }
+
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'techcamp-chat-space.com'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/techcamp-chat-space.com'
+    when 'staging'
+      config.fog_directory = 'stg.techcamp-chat-space.com'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/stg.techcamp-chat-space.com'
+    when 'development'
+      config.fog_directory = 'dev.techcamp-chat-space.com'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dev.techcamp-chat-space.com'
+    when 'test'
+      config.fog_directory = 'test.techcamp-chat-space.com'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/test.techcamp-chat-space.com'
+  end
+end
