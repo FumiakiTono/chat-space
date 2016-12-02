f940c214ead02b0889114ee40b046af8f3c2bba9
FumiakiTono <tonotorious@gmail.com>
2016-12-02 19:05:06 +0900

    Add when "test" instead of unless

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,31 +1,26 @@
-unless Rails.env.test?
-  CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_credentials = {
-        :provider               => 'AWS',
-        :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-        :region                 => ENV['AWS_REGION'],
-        :path_style             => true,
-    }
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => ENV['AWS_REGION'],
+      :path_style             => true,
+  }
 
-    config.fog_public     = true
-    config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
 
-    case Rails.env
-      when 'production'
-        config.fog_directory = 'techcamp-chat-space.com'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/techcamp-chat-space.com'
-      when 'staging'
-        config.fog_directory = 'stg.techcamp-chat-space.com'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/stg.techcamp-chat-space.com'
-      when 'development'
-        config.fog_directory = 'dev.techcamp-chat-space.com'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dev.techcamp-chat-space.com'
-    end
-  end
-else
-  CarrierWave.configure do |config|
-    config.storage = :file
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'techcamp-chat-space.com'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/techcamp-chat-space.com'
+    when 'staging'
+      config.fog_directory = 'stg.techcamp-chat-space.com'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/stg.techcamp-chat-space.com'
+    when 'development'
+      config.fog_directory = 'dev.techcamp-chat-space.com'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dev.techcamp-chat-space.com'
+    when "test"
+      config.storage = :file
   end
 end
