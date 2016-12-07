c4f27054ca1c8a39ee6031233c76850c7c99f662
FumiakiTono <tonotorious@gmail.com>
2016-12-07 17:18:40 +0900

    Revise image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,8 +2,15 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
+  storage :fog
+  include CarrierWave::RMagick
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    process :resize_to_limit => [300, 300]
   end
 
 end
