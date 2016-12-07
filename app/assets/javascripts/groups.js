ed69d8f56243dec5e67f35dccd6b6dfc214f82eb
FumiakiTono <tonotorious@gmail.com>
2016-12-07 17:27:14 +0900

    Do refectring at group.js

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -25,42 +25,44 @@ $(function(){
     return list;
   };
 
-  $("#group_user_ids").on("keyup", function(e){
-    e.preventDefault();
-    var input = $("#group_user_ids").val();
-    var params = { name: input };
-    $.ajax({
-      url: "/groups/new.json",
-      type: "GET",
-      data: params,
-      dataType: "json"
-    })
-      .done(function(datas){
-        var word = "^"+input;
-        var reg = RegExp(word);
-        var preWord;
-        $(".chat-group-user").remove();
-        if(word!=preWord && input.length!==0){
-          $.each(datas, function(i, data){
-            if(data.match(reg)){
-              $("#user-search-result").append(addList_btn(i, data));
-              $("#user-search-add-"+i).on("click", function(e){
-                e.preventDefault();
-                var input = $("#chat-group-user-"+i).find(".chat-group-user__name").text();
-                $("#chat-group-users").append(addMember_btn(i, input));
-                $("#chat-group-user-"+i).remove();
-                $("#user-search-remove-"+i).on("click", function(e){
-                    e.preventDefault();
-                    $("#chat-group-member-"+i).remove();
-                })
-              });
-            }
-          })
-        }
-        preWord = word;
+  // $(document).on("turbolinks:load", function(){
+    $("#group_user_ids").on("keyup", function(e){
+      e.preventDefault();
+      var input = $("#group_user_ids").val();
+      var params = { name: input };
+      $.ajax({
+        url: "/groups/new.json",
+        type: "GET",
+        data: params,
+        dataType: "json"
       })
-      .fail(function(data){
-        alert("エラーが発生しました。");
-      })
-  });
+        .done(function(datas){
+          var word = "^"+input;
+          var reg = RegExp(word);
+          var preWord;
+          $(".chat-group-user").remove();
+          if(word!=preWord && input.length!==0){
+            $.each(datas, function(i, data){
+              if(data.match(reg)){
+                $("#user-search-result").append(addList_btn(i, data));
+                $("#user-search-add-"+i).on("click", function(e){
+                  e.preventDefault();
+                  var input = $("#chat-group-user-"+i).find(".chat-group-user__name").text();
+                  $("#chat-group-users").append(addMember_btn(i, input));
+                  $("#chat-group-user-"+i).remove();
+                  $("#user-search-remove-"+i).on("click", function(e){
+                      e.preventDefault();
+                      $("#chat-group-member-"+i).remove();
+                  })
+                });
+              }
+            })
+          }
+          preWord = word;
+        })
+        .fail(function(data){
+          alert("エラーが発生しました。");
+        })
+    });
+  // })
 });
