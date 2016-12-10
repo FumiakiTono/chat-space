3321a3bdafd95caf0a2e07130433be08f0c38f5d
FumiakiTono <tonotorious@gmail.com>
2016-12-10 19:43:39 +0900

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "groups#index"
+  devise_for :users
   resources :groups, only: [:new, :create, :edit, :update, :index] do
     resources :messages, only: [:index, :create]
   end
