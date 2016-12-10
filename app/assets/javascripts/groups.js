35eba088a99a6ae82d9814366b9b46ea8004051f
FumiakiTono <tonotorious@gmail.com>
2016-12-10 18:08:15 +0900

    Modify incrementalsearch and user_id

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,34 +1,36 @@
+function addMemberBtn(id, name){
+  member =
+    "<div class=chat-group-user id=chat-group-member-"+ id +">"     +
+      "<input type='hidden' name='group[user_ids][]' value= " + id + ">" +
+      "<p class=chat-group-user__name>"                            +
+        name                                                       +
+      "</p>"                                                       +
+      "<a class=user-search-remove id=user-search-remove-"+ id +">" +
+        "削除"                                                      +
+      "</a>"                                                       +
+    "</div>"
+  return member;
+}
+
+function addListBtn(id, name){
+  list =
+    "<div class=chat-group-user id=chat-group-user-"+ id +">" +
+      "<p class=chat-group-user__name>"                      +
+        name                                                 +
+      "</p>"                                                 +
+      "<a class=user-search-add id=user-search-add-"+ id +">" +
+        '追加'                                                +
+      "</a>"                                                 +
+    "</div>"
+  return list;
+};
+
 $(function(){
-  function addMember_btn(i,name){
-    member =
-      "<div class=chat-group-user id=chat-group-member-"+ i +">"     +
-        "<p class=chat-group-user__name>"                            +
-          name                                                       +
-        "</p>"                                                       +
-        "<a class=user-search-remove id=user-search-remove-"+ i +">" +
-          "削除"                                                      +
-        "</a>"                                                       +
-      "</div>"
-    return member;
-  }
 
-  function addList_btn(i,name){
-    list =
-      "<div class=chat-group-user id=chat-group-user-"+ i +">" +
-        "<p class=chat-group-user__name>"                      +
-          name                                                 +
-        "</p>"                                                 +
-        "<a class=user-search-add id=user-search-add-"+ i +">" +
-          '追加'                                                +
-        "</a>"                                                 +
-      "</div>"
-    return list;
-  };
-
-  // $(document).on("turbolinks:load", function(){
-    $("#group_user_ids").on("keyup", function(e){
+  $(document).on("turbolinks:load", function(){
+    $("#group-users").on("keyup", function(e){
       e.preventDefault();
-      var input = $("#group_user_ids").val();
+      var input = $("#group-users").val();
       var params = { name: input };
       $.ajax({
         url: "/groups/new.json",
@@ -40,19 +42,19 @@ $(function(){
           var word = "^"+input;
           var reg = RegExp(word);
           var preWord;
-          $(".chat-group-user").remove();
           if(word!=preWord && input.length!==0){
             $.each(datas, function(i, data){
-              if(data.match(reg)){
-                $("#user-search-result").append(addList_btn(i, data));
-                $("#user-search-add-"+i).on("click", function(e){
+              $("#chat-group-user-" + data.id).remove();
+              if(data.name.match(reg)){
+                $("#user-search-result").append(addListBtn(data.id, data.name));
+                $("#user-search-add-"+data.id).on("click", function(e){
                   e.preventDefault();
-                  var input = $("#chat-group-user-"+i).find(".chat-group-user__name").text();
-                  $("#chat-group-users").append(addMember_btn(i, input));
-                  $("#chat-group-user-"+i).remove();
-                  $("#user-search-remove-"+i).on("click", function(e){
+                  var input = $("#chat-group-user-"+data.id).find(".chat-group-user__name").text();
+                  $("#chat-group-users").append(addMemberBtn(data.id, input));
+                  $("#chat-group-user-"+data.id).remove();
+                  $("#user-search-remove-"+data.id).on("click", function(e){
                       e.preventDefault();
-                      $("#chat-group-member-"+i).remove();
+                      $("#chat-group-member-"+data.id).remove();
                   })
                 });
               }
@@ -60,9 +62,9 @@ $(function(){
           }
           preWord = word;
         })
-        .fail(function(data){
+        .fail(function(datas){
           alert("エラーが発生しました。");
         })
     });
-  // })
+  })
 });
