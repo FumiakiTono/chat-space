e78c0e5efafde281b5b831640be582361fb8e7d2
FumiakiTono <tonotorious@gmail.com>
2016-12-13 18:20:04 +0900

    Change the ssh key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@
 server '35.164.215.254', user: 'fumiaki', roles: %w{app db web}
 
 #デプロイするサーバにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/first_aws_rsa'
+set :ssh_options, keys: '~/.ssh/github_rsa'
